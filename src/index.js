f960290a89faac10cb638f3621ad3d19b14eb905
DmitriyAxi <48443463+DmitriyAxi@users.noreply.github.com>
2020-10-02 15:33:11 +0300

    feat: fix task typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,15 +1,43 @@
-
 exports.min = function min (array) {
-  if (!array || !array.length) return 0;
-  return Math.min(...array);
+  if((array != null) && (array.length != 0))
+  {
+    let min = array[0];
+    for(var i = 1; i < array.length; i++)
+    {
+      if (array[i] < min)
+        min = array[i];
+    }
+    return min;
   }
+  else
+    return 0;
+}
 
 exports.max = function max (array) {
-  if (!array || !array.length) return 0;
-  return Math.max(...array);
+  if((array != null) && (array.length != 0))
+  {
+    let max = array[0];
+    for (var i = 1; i < array.length; i++)
+    {
+      if (array[i] > max)
+      max = array[i];
+   }
+  return max;
+  }
+  else
+    return 0;
 }
 
 exports.avg = function avg (array) {
-  if (!array || !array.length) return 0;
-  return array.reduce((accumulator, currentValue) => accumulator + currentValue, 0) / array.length;
+  if((array != null) && (array.length != 0))
+  {
+    let avg = 0;
+    for (var i = 0; i < array.length; i++)
+    {
+      avg += array[i];
+    }
+    return avg / array.length;
+  }
+  else
+    return 0;
 }
